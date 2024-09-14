c0de0b02180c27c2af4ec238826c0ecb86d50754
HelenaDEspindula <helena.despindula@gmail.com>
2024-09-14 09:11:14 -0300

    14-SET-2024--09:11

---
+++ 07-Processamento_de_Big_Data/escolas.sql
@@ -0,0 +1,11 @@
+-- !preview conn=DBI::dbConnect(RSQLite::SQLite())
+
+CREATE TABLE escola (
+  codigo INT PRIMARY KEY,
+  nome TEXT NOT NULL,
+  cod_municipio INT NOT NULL,
+  endereco TEXT NULL,
+  compl_endereco TEXT NULL,
+  bairro TEXT NULL,
+  FOREIGN KEY (cod_municipio) REFERENCES municipio(codigo)
+);
